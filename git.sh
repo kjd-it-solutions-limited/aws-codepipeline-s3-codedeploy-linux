ed485f02c29d2c185a1f809d4c69fbcc76108c25
git <git@Kens-MacBook-Pro.local>
2019-05-27 21:44:36 +0100

    updated on Mon May 27 21:44:36 BST 2019

--- git.sh
+++ git.sh
@@ -1,12 +1,11 @@
    cd ~/aws-codepipeline-s3-codedeploy-linux/
    git add *
-   git commit -m "test"
+    echo "ken@kjdsolutions.co.uk"
+        echo "Yassmine22"git commit -m "test"
    git push 
    git status
    git add *
    git status
    mess=`date`
-	echo "ken@kjdsolutions.co.uk"
-        echo "Yassmine22"
    git commit -m "updated on $mess"
    git status
