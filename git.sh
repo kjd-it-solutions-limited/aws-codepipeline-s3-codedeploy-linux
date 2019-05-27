277f517c0c66af7ab46f0ba6987dce3ef058e652
git <git@Kens-MacBook-Pro.local>
2019-05-27 21:09:48 +0100

    test

---
+++ git.sh
@@ -0,0 +1,9 @@
+   cd ~/aws-codepipeline-s3-codedeploy-linux/
+   git add *
+   git commit -m "test"
+   git push 
+   git status
+   git add *
+   git status
+   git commit -m "updated on `date`"
+
